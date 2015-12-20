e6afa4cb9330d57ea8e7e6c05fa135c7963c5b7c
Steve Mayne <steve.mayne@gmail.com>
2015-12-20 15:00:46 +0000

    Added root password-setting functionality and localised the mysql source

--- Dockerfile
+++ Dockerfile
@@ -9,19 +9,19 @@ RUN apt-get update && apt-get install -q -y \
 
 RUN groupadd -r mysql && useradd -r -g mysql mysql
 
-ADD https://www.pccc.com/downloads/apache/current/mysql-4.1.25.tar.gz /tmp/mysql-4.1.24.tar
-RUN tar -xvf /tmp/mysql-4.1.24.tar --directory=/usr/local/src
+#ADD https://www.pccc.com/downloads/apache/current/mysql-4.1.25.tar.gz /tmp/mysql-4.1.24.tar
+#RUN tar -xvf /tmp/mysql-4.1.24.tar --directory=/usr/local/src
+
+ADD mysql-4.1.25.tar.gz /usr/local/src
 
 ENV PATH /usr/local/mysql/bin:$PATH
 ENV MYSQLDATA /usr/local/mysql/var
 VOLUME /usr/local/mysql/var
 
 COPY buildmysql4.sh /tmp/buildmysql4.sh
-RUN chmod +x /tmp/buildmysql4.sh
 RUN /tmp/buildmysql4.sh
 
 COPY docker-entrypoint.sh /
-RUN chmod +x /docker-entrypoint.sh
 
 EXPOSE 3306
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,46 @@ if [ "$1" = 'mysqld_safe' ]; then
     chown -R mysql "$MYSQLDATA"
 	
     if [ -z "$(ls -A "$MYSQLDATA")" ]; then
+    	echo "Initializing database..."
         mysql_install_db --user=mysql
+        
+        if [ "$MYSQL_ROOT_PASSWORD" == "" ]; then
+			echo >&2 'error: database is uninitialized and password option is not specified '
+			echo >&2 '  You need to specify MYSQL_ROOT_PASSWORD'
+			exit 1
+		else
+        	echo "Setting root password..."
+
+        	mysqld_safe --skip-networking &
+			pid="$!"
+			
+			mysql=( mysql --protocol=socket -uroot )
+
+			for i in {30..0}; do
+				if echo 'SELECT 1' | "${mysql[@]}" &> /dev/null; then
+					break
+				fi
+				echo 'MySQL init process in progress...'
+				sleep 1
+			done
+
+			if [ "$i" = 0 ]; then
+				echo >&2 'MySQL init process failed.'
+				exit 1
+			fi
+
+"${mysql[@]}" <<-EOSQL
+	SET @@SESSION.SQL_LOG_BIN=0;
+	DELETE FROM mysql.user ;
+	GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+	FLUSH PRIVILEGES ;
+EOSQL
+
+			if ! kill -s TERM "$pid" || ! wait "$pid"; then
+				echo >&2 'MySQL init process failed.'
+				exit 1
+			fi
+        fi
     fi
 fi
 
